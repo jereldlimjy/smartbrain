0b40646aa8f3bb921ff8a490d0ddf9bc6932f537
Jereld Lim <jereldlimjy@hotmail.com>
2020-05-03 17:45:02 +0800

    Updates

---
+++ precache-manifest.a2c01e457baa7b9115de4d40ca22ee6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633b2f9914459b97e244d850f0ff7674",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "10f80b64b2f96ab49880",
+    "url": "/smartbrain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "8816381577929a9fec42",
+    "url": "/smartbrain/static/css/main.123c08de.chunk.css"
+  },
+  {
+    "revision": "10f80b64b2f96ab49880",
+    "url": "/smartbrain/static/js/2.16423424.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smartbrain/static/js/2.16423424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8816381577929a9fec42",
+    "url": "/smartbrain/static/js/main.9be9032f.chunk.js"
+  },
+  {
+    "revision": "a6176b28c0a0e00021a3",
+    "url": "/smartbrain/static/js/runtime-main.201b3203.js"
+  }
+]);
