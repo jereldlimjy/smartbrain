6be21b5616c2c0236e308d2a28f0e36552efaf78
Jereld Lim <jereldlimjy@hotmail.com>
2020-05-03 16:15:10 +0800

    smartbrain first commit

---
+++ src/App.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 50,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+}
+
+const initialState = {
+	input: '',
+	imageUrl: '',
+	box: {},
+	route: 'signin',
+	isSignedIn: false,
+	user: {
+		id: '',
+		name: '',
+		email: '',
+		entries: 0,
+		joined: ''
+	}
+}
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = initialState;
+	}
+
+	loadUser = (data) => {
+		this.setState({user: {
+			id: data.id,
+			name: data.name,
+			email: data.email,
+			entries: data.entries,
+			joined: data.joined
+		}})
+	}
+
+	onRouteChange = (route) => {
+		if (route === 'signout') {
+			this.setState(initialState);
+		} else if (route === 'home') {
+			this.setState({isSignedIn: true});
+		}
+		this.setState({route: route});
+	}
+
+	calculateFaceLocation = (data) => {
+		console.log(data);
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		console.log(clarifaiFace);
+		const image = document.getElementById('inputImage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			rightCol: width - (clarifaiFace.right_col * width),
+			topRow: clarifaiFace.top_row * height,
+			bottomRow: height - (clarifaiFace.bottom_row * height)
+		}
+	}
+
+	displayFaceBox = (box) => {
+		this.setState({ box: box });
+	}
+
+	onInputChange = (event) => {
+		this.setState({ input: event.target.value });
+	}
+
+	onButtonSubmit = (event) => {
+		this.setState({ imageUrl: this.state.input })
+		fetch('https://arcane-waters-83680.herokuapp.com/imageurl', {
+			method: 'post',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				input: this.state.input
+			})
+		})
+		.then(response => response.json())
+		.then(response => {
+			if (response) {
+				fetch('https://arcane-waters-83680.herokuapp.com/image', {
+					method: 'put',
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify({
+						id: this.state.user.id
+					})
+				})
+				.then(response => response.json())
+				.then(count => {
+					this.setState(Object.assign(this.state.user, { entries: count }));
+				})
+				.catch(console.log)
+			}
+			this.displayFaceBox(this.calculateFaceLocation(response));
+		})
+		.catch(err => console.log(err))
+	}
+
+	render() {
+	    return (
+	    	<div className="App">
+	      		<Particles className='particles' 
+		        	params={particlesOptions}
+		    	/>
+		    	<Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+		    	{ this.state.route === 'home' ?
+		    		<div>
+			        	<Logo />
+			        	<Rank name={this.state.user.name} entries={this.state.user.entries} />
+			        	<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+			        	<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+			        </div>
+	        		: ( this.state.route === 'signin' ? 
+	        			<SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+	        			:
+	        			<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+	        		)
+		        }
+	      	</div>
+	    );
+	}
+}
+
+export default App;
